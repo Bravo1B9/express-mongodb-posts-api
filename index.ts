af39584fd63f1967725d5eb01d264fe4a0853a39
Bravo1B9 <brofe93@gmail.com>
2023-04-08 11:03:08 +0000

    Setup and establish database connection

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 require('dotenv').config();
+import { connectToDatabase } from "./db";
 const express = require('express');
 
 const port = process.env.PORT;
 
 const app = express();
+
+connectToDatabase();
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
