7d753698b5cec507542a02c3f5a9927ad43d2abc
Bravo1B9 <brofe93@gmail.com>
2023-04-09 06:18:07 +0000

    Refactor

--- model/postModel.ts
+++ model/postModel.ts
@@ -18,7 +18,7 @@ export const getAllPosts = async () => {
   return await postCollection
     .aggregate([
       {
-        $project: { title: 1, body: 1, upvotes: 1, downvotes: 1, createdAt: 1 },
+        $project: { title: 1, body: 1, upvotes: 1, downvotes: 1 },
       },
       { $sort: { createdAt: -1 } },
       { $limit: 3 },
